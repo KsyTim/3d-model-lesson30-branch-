506e3f668e2be0167c802914fa1a2488ed505560
KsyTim <timoshenko.ksenia.1998@gmail.com>
2021-01-29 19:13:27 +0400

    assigned values to variables

---
+++ lesson01/js/main.js
@@ -0,0 +1,18 @@
+//lesson 01
+let money, 
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+    
+alert('first');
+console.log('second');
+
+//lesson 02 simple
+money = 40000;
+income = 'freelance';
+addExpenses = 'fees, medicines, entertains';
+deposit = false;
+mission = 400000;
+period = 8;
