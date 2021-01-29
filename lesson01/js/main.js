59800a2440cb16bc69697f58e359eba0294740e4
KsyTim <timoshenko.ksenia.1998@gmail.com>
2021-01-29 19:38:05 +0400

    practiced variables properities & methods

--- lesson01/js/main.js
+++ lesson01/js/main.js
@@ -9,10 +9,3 @@ let money,
 alert('first');
 console.log('second');
 
-//lesson 02 simple
-money = 40000;
-income = 'freelance';
-addExpenses = 'fees, medicines, entertains';
-deposit = false;
-mission = 400000;
-period = 8;
